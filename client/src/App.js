d0afe312d87e417ec24dfa9cac7de0e84b6c744a
Boris Kljajic <ibanez1222@gmail.com>
2022-06-24 14:19:15 +0200

    add chat room and join from client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
-import "./App.css";
-const App = () => {
+import {io} from "socket.io-client";
+
+import './App.css'
+
+
+const SOCKET_SERVER_URL = "http://localhost:8000";
+const ROOM = "GENERAL";
+const socket = io(SOCKET_SERVER_URL, {autoConnect: false});
+socket.onAny((event, ...args) => {
+  console.log(event, args);
+});
+socket.connect();
+socket.emit("join_room", ROOM)
+
+function App() {
   return (
     <div>
       <h1 className='heading__one'>React application!!!</h1>
     </div>
   );
-};
+}
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -20,6 +20,15 @@ io.on("connection", (socket) => {
   }
   console.log('connected')
 
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
   socket.on("disconnect", () => {
     socket.broadcast.emit("user disconnected", socket.id);
   });
