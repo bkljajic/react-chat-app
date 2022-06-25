7fca8f59821eec0dc6361b578ba9e54218823091
Boris Kljajic <ibanez1222@gmail.com>
2022-06-25 08:20:48 +0200

    add header component

---
+++ client/src/components/header/ChatHeader.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./ChatHeader.css";
+
+const ChatHeader = ({ title }) => {
+  return <div className="chat-header">{title}</div>;
+};
+
+export default ChatHeader;
+
