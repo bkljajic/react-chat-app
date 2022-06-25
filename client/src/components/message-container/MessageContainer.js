37c2c36803c7118c9f2ff94495649d983cfbbdb4
Boris Kljajic <ibanez1222@gmail.com>
2022-06-25 11:18:02 +0200

    handleSendMessage and handleNewMessageChange functions add
    MessageContainer is reformated and correct props are passed, functions for sending and receiving message on socket are added, chat screen is formed using previously created components

--- client/src/components/message-container/MessageContainer.js
+++ client/src/components/message-container/MessageContainer.js
@@ -1,6 +1,7 @@
-import Message from "../message/Message";
 import React, { useEffect, useRef } from "react";
-import "../ChatRoom.css";
+
+import Message from "../message/Message";
+import './MessageContainer.css'
 
 const MessageContainer = ({ messages }) => {
   const messageRef = useRef();
@@ -21,7 +22,7 @@ const MessageContainer = ({ messages }) => {
         {messages.map((message, i) => (
           <Message
             key={i}
-            ownership={...message}
+            {...message}
           />
         ))}
       </div>
--- client/src/pages/ChatRoom.js
+++ client/src/pages/ChatRoom.js
@@ -1,11 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
+import MessageContainer from "../components/message-container/MessageContainer";
+import MessageTextArea from "../components/message-text-area/MessageTextArea";
+import Button from "../components/button/Button";
+import ChatHeader from "../components/header/ChatHeader";
 import "./ChatRoom.css";
 
 const ChatRoom = ({ socket, room }) => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState([]);
 
-  return <div className="chat-room-container"></div>;
+  const handleNewMessageChange = (event) => {
+    setNewMessage(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    const message = {
+      content: newMessage,
+      userId: socket.id,
+      ownMessage: true,
+      timestamp: Date.now(),
+      room: room,
+      id: uuidv4(),
+    };
+    socket.emit("send_message", message);
+    setMessages((list) => [...list, message]);
+    setNewMessage("");
+  };
+
+  useEffect(() => {
+    socket.off("receive_message").on("receive_message", (data) => {
+      setMessages((list) => [
+        ...list,
+        { ...data, ownMessage: socket.id == data.userId },
+      ]);
+    });
+  }, [socket]);
+
+  return (
+    <div className="chat-room-container">
+      <ChatHeader title={""} />
+      <MessageContainer messages={messages} />
+      <MessageTextArea
+        message={newMessage}
+        handleMessageChange={handleNewMessageChange}
+        placeholder={"Write message..."}
+      />
+      <Button
+        text={"Send"}
+        clickHandler={handleSendMessage}
+        disabled={newMessage.length < 1}
+      />
+    </div>
+  );
 };
 export default ChatRoom;
